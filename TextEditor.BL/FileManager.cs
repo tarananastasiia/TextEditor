ff10fce87708738d815efdd76bbbe38082ad66ba
vhaliaha <vhaliaha@deloitte.com>
2020-09-21 13:27:27 +0300

    Add project files.

---
+++ TextEditor.BL/FileManager.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Text;
+
+namespace TextEditor.BL
+{
+    public class FileManager
+    { 
+        public string GetContent(string filePath,Encoding encoding)
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;
+        }
+    }
+}
